491dfafdc4cd0b9500d503874144f3e5f5573f37
fredukita <fredukita@gmail.com>
2012-12-27 01:49:55 -0300

    12.27.2012 1:49am

--- Assets/_Scripts/ParticlesManager.js
+++ Assets/_Scripts/ParticlesManager.js
@@ -1,16 +1,21 @@
 #pragma strict
 
-var Stone:Rigidbody;
+var Wreck:Rigidbody;
+var NumberOfPieces:int = 10;
+
 var Particles : ParticleEmitter;
 var ParticlesTimeLive:float = Mathf.Infinity;
 var OnCollision : boolean = false;
+
+private var piece: Rigidbody;
+private var rndVel:Vector3;
 
 function Start(){
 	if(!OnCollision){
 		gameObject.collider.enabled = false;
 		var particleClone : ParticleEmitter = Instantiate (Particles, transform.position, transform.rotation);
 		particleClone.Emit();
-		destroyParticles(particleClone,ParticlesTimeLive);
+		Destroy(particleClone.gameObject,ParticlesTimeLive);
 	}
 		
 }
@@ -20,44 +25,32 @@ function OnCollisionEnter(collision : Collision) {
 	particleClone.transform.position = collision.contacts[0].point;
 	particleClone.transform.rotation = Quaternion.FromToRotation(Vector3.up, collision.contacts[0].normal);	
 	particleClone.Emit();
-	//destroyParticles(particleClone,ParticlesTimeLive);
-	Destroy(particleClone,ParticlesTimeLive);
+	Destroy(particleClone.gameObject,ParticlesTimeLive);
 	
 	StartCoroutine(wrecker(collision));
 	
 	
 }
 
-function wrecker(collision:Collision){
-	for (var i=0; i<5; i++) {
+function wrecker(collision:Collision){	
+	
+	for (var i=0; i<NumberOfPieces; i++) {
 
-        var aStone : Rigidbody = Instantiate(Stone, collision.contacts[0].point, Quaternion.identity);
-        
-        //aStone.collider.enabled=false;
-        Physics.IgnoreCollision(aStone.collider, transform.root.collider);
-        
-		var y = Random.Range(-5.0, 5.0);
+		piece = Instantiate(Wreck, collision.contacts[0].point, Quaternion.identity);		
 		
-		var z = Random.Range(0.0, 5.0);
+		Physics.IgnoreCollision(piece.collider, transform.root.collider);		
+			
+		piece.transform.rotation = Quaternion.FromToRotation(Vector3.up, collision.contacts[0].normal);
 		
+		rndVel = Vector3( 0,Random.Range(-5.0, 10.0),0);
+		
+		piece.rigidbody.velocity = -collision.contacts[0].normal.normalized + rndVel;		
+		
+		Destroy(piece.gameObject,ParticlesTimeLive);
+		
+		//yield;
 
-		        
-
-        aStone.velocity = collision.contacts[0].normal * -z;
-        
-        aStone.velocity.y = y;
-        
-        Destroy(aStone.gameObject,ParticlesTimeLive);
-        
-        //yield;
-
-    }
+	}
     
     yield;
-}
-
-function destroyParticles(particleClone : ParticleEmitter, time:float) {
-    yield WaitForSeconds(time);
-    if(particleClone)
-		Destroy(particleClone.gameObject);
 }
